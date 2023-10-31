2bb79906049a8dc22708afcc6f15c6aaf945d6b8
AhmadKhan02 <kahmadshakeel@gmail.com>
2023-10-31 10:21:43 +0530

    .

--- options/options.js
+++ options/options.js
@@ -1,24 +1,24 @@
+// Set the API URL
+const apiUrl = "https://your-api-url.com";
+
 // Function to save the API URL to Chrome storage
 function saveAPIUrl() {
-    const apiUrl = document.getElementById("api-url").value;
-  
+    // Save the API URL (if needed)
     chrome.storage.sync.set({ apiUrl: apiUrl }, () => {
-      // Notify the user that the API URL has been saved
-      const status = document.getElementById("status");
-      status.textContent = "API URL saved!";
+        // Notify the user that the API URL has been saved
+        const status = document.getElementById("status");
+        status.textContent = "API URL saved!";
     });
-  }
-  
-  // Function to restore the API URL from Chrome storage
-  function restoreOptions() {
-    chrome.storage.sync.get({ apiUrl: "" }, (items) => {
-      document.getElementById("api-url").value = items.apiUrl;
-    });
-  }
-  
-  // Add a click event listener to the "Save" button
-  document.getElementById("save-button").addEventListener("click", saveAPIUrl);
-  
-  // Restore the API URL when the options page is loaded
-  document.addEventListener("DOMContentLoaded", restoreOptions);
-  
+}
+
+// Function to display the API URL
+function displayAPIUrl() {
+    const apiURLSpan = document.getElementById("api-url");
+    apiURLSpan.textContent = apiUrl;
+}
+
+// Add a click event listener to the "Save" button
+document.getElementById("save-button").addEventListener("click", saveAPIUrl);
+
+// Display the API URL when the options page is loaded
+document.addEventListener("DOMContentLoaded", displayAPIUrl);
