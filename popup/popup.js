6a796786a3694df2e9c2c7f2e56a257281429d85
AnshOhri <anshohri4155@gmail.com>
2023-10-31 12:42:53 +0530

    changes in popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,26 +1,36 @@
 // Function to update the status message in the popup
 function updateStatus(status) {
-    const statusElement = document.getElementById("status");
-    statusElement.textContent = status;
-  }
-  
-  // Function to check the current tab's URL and update the popup
-  function checkCurrentTab() {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const currentTab = tabs[0];
-      const currentURL = currentTab.url;
-  
-      // Send the URL to your server for analysis (you should replace this with your server URL)
-      // Once you receive the result, update the status accordingly
-      updateStatus("Checking...");
-    });
-  }
-  
-  // Add a click event listener to the extension icon
-  chrome.action.onClicked.addListener(() => {
-    checkCurrentTab();
-  });
-  
-  // Check the current tab when the extension is loaded
-  checkCurrentTab();
-  
+	const statusElement = document.getElementById('status');
+	statusElement.textContent = status;
+}
+
+// Function to check the current tab's URL and update the popup
+function checkCurrentTab() {
+	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+		const currentTab = tabs[0];
+		const currentURL = currentTab.url;
+
+		// Send the URL to your server for analysis (you should replace this with your server URL)
+		// Once you receive the result, update the status accordingly
+		updateStatus('Checking...');
+	});
+}
+
+// Add a click event listener to the extension icon
+chrome.action.onClicked.addListener(() => {
+	checkCurrentTab();
+});
+
+// Check the current tab when the extension is loaded
+checkCurrentTab();
+
+document.addEventListener('DOMContentLoaded', function () {
+	// Get a reference to the close button by its id
+	const closeButton = document.getElementById('close-button');
+
+	// Add a click event listener to the close button
+	closeButton.addEventListener('click', function () {
+		// Close the extension popup
+		window.close();
+	});
+});
